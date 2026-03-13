9661ddf937a9e10d07656b7e597e48a82785e250
Phron Ya <phornya26@gmail.com>
2026-03-13 09:04:29 +0700

    update and create document for backend

--- src/auth/services/apiClient.js
+++ src/auth/services/apiClient.js
@@ -1,1 +1,7 @@
-export { API_BASE_URL, apiClient, authApiClient } from "../../lib/apiClient";
+export {
+  API_BASE_URL,
+  API_TIMEOUT_MS,
+  DEFAULT_API_BASE_URL,
+  apiClient,
+  authApiClient,
+} from "../../lib/apiClient";
